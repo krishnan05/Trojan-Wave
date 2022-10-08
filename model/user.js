4e026a94e766657bbc0c8dd2db8e78cd6f276554
Aayush Krishnan <ayush05012003krishn@gmail.com>
2022-10-08 18:24:39 +0530

    Employee dashboard

--- model/user.js
+++ model/user.js
@@ -1,3 +1,4 @@
+const { string } = require("i/lib/util");
 const Mongoose = require("mongoose");
 
 const UserSchema =({
@@ -10,6 +11,27 @@ const UserSchema =({
     type: String,
     minlength: 6,
     required: true,
+  },
+  Mail_ID:{
+    type:String,
+    trim:true,
+    lowercase:true,
+    unique:true,
+    required:true,
+    
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    immutable:true
+
+  },
+  ContactNumber:{
+    type:Number,
+    length:10
+  },
+  Department:{
+    type:String
+  },
+  JoiningDate:{
+    type:Date
   }
 
 });
